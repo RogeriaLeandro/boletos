3bb9347e73dc7f5b85910d3fc352e52274c4f615
RogeriaLeandro <RogeriaLeandro>
2023-09-02 11:38:27 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/boletos/BoletosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.boletos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoletosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
