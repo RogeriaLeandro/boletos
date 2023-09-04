0095d91979024982889bbb868eb393ae58bc3de3
RogeriaLeandro <RogeriaLeandro>
2023-09-03 22:25:09 -0300

    Valida se Associado está Cadastrado em Associados

--- src/main/java/br/com/boletos/handler/BoletosErrorHandler.java
+++ src/main/java/br/com/boletos/handler/BoletosErrorHandler.java
@@ -1,5 +1,6 @@
 package br.com.boletos.handler;
 
+import br.com.boletos.exceptions.AssociadoNaoExisteNaAPIException;
 import br.com.boletos.exceptions.BoletoPagoException;
 import br.com.boletos.exceptions.DataVencimentoAposDataAtualException;
 import br.com.boletos.exceptions.ValorDeBoletoDivergenteNoPagamentoException;
@@ -13,8 +14,12 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class BoletosErrorHandler {
 
-    @ExceptionHandler(value = {BoletoPagoException.class, DataVencimentoAposDataAtualException.class, ValorDeBoletoDivergenteNoPagamentoException.class})
-    public ResponseEntity<Object> catchTipoPessoaInconsistenteException(RuntimeException ex, WebRequest request) {
+
+    @ExceptionHandler(value = {BoletoPagoException.class,
+                                DataVencimentoAposDataAtualException.class,
+                                ValorDeBoletoDivergenteNoPagamentoException.class,
+                                AssociadoNaoExisteNaAPIException.class})
+    public ResponseEntity<Object> catchBoleto(RuntimeException ex, WebRequest request) {
         return new ResponseEntity<Object>(ex.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 }
